9aa8f547cbbfc7ede8606f4ce217bfd71f77e445
Oleg <oleg.kalinovskiy2014@gmail.com>
2019-10-03 13:32:12 +0300

    Сделал footer

--- local/templates/divier/footer.php
+++ local/templates/divier/footer.php
@@ -83,6 +83,28 @@
 				false
 			);?>
 			</div>
-		</div>	
+		</div>
+
+<section class="footer">
+    <div class="container">
+        <div class="footer-wrapper">
+            <div class="footer-logo">
+                <a href="<?= SITE_DIR ?>">
+                    <img src="<?= SITE_TEMPLATE_PATH ?>/images/svg/logo2.svg" alt="">
+                </a>
+            </div>
+            <div class="footer-address">
+                <img class="address" src="<?= SITE_TEMPLATE_PATH ?>/images/svg/address2.svg">
+                <p>105064, г. Москва, ул. Земляной Вал, 9, этаж 4, офис 4065</p>
+            </div>
+            <div class="footer-dev-info">
+                <p>Общество с ограниченной ответственностью «Техскан-Сервис»,</p>
+                <p>ИНН: 7724406801 / 770901001, БИК: 044525700, Кор/счет:  30101810200000000700</p>
+                <p class="dev"><a href="#">Создание сайта </a> ДиВиЕР</p>
+            </div>
+
+        </div>
+    </div>
+</section>
 </body>
 </html>
