9f2f584173e15cdda95165b56fbec208c425e4bc
Oleg <oleg.kalinovskiy2014@gmail.com>
2019-10-14 19:05:16 +0300

    Все по не многу

--- .top.menu.php
+++ .top.menu.php
@@ -2,7 +2,7 @@
 $aMenuLinks = Array(
     Array(
         "О компании",
-        "#",
+        "/about/",
         Array(),
         Array(),
         ""
--- contacts/index.php
+++ contacts/index.php
@@ -1,50 +1,78 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Контактная информация");
 ?>
-<p>Обратитесь к нашим специалистам и получите профессиональную консультацию по услугам нашего банка.</p>
+    <section class="services-card">
+        <div class="container">
+            <div class="title-38s mb-45">
+                Контакты
+            </div>
 
-<p>Вы можете обратиться к нам по телефону, по электронной почте или договориться о встрече в нашем офисе. Будем рады помочь вам и ответить на все ваши вопросы. </p>
+        </div>
+    </section>
+    <section class="map">
+        <div id="map">
+        </div>
+        <div class="map-content">
+            <div class="container">
+                <div class="map-content-block">
+                    <img src="/local/templates/divier/images/svg/logo.svg">
+                    <div class="map-contacts">
+                        <div class="map-contacts-flex">
+                            <img src="/local/templates/divier/images/svg/address.svg">
+                            <p>
+                                ул. Земляной Вал, 9, этаж 4, офис 4065 (БЦ Ситидел), Москва, Россия
+                            </p>
+                        </div>
+                        <div class="map-contacts-flex">
+                            <img src="/local/templates/divier/images/svg/phone2.svg"> <a href="tel:+74955043626"
+                                                                                         class="map-phone">+7 (495)
+                                504-36-26</a>
+                        </div>
+                        <div class="map-contacts-flex">
+                            <img src="/local/templates/divier/images/svg/phone2.svg"> <a href="tel:+79662722215"
+                                                                                         class="map-phone">+7 (966)
+                                272-22-15</a>
+                        </div>
+                        <div class="map-contacts-flex">
+                            <img src="/local/templates/divier/images/svg/email2.svg"> <a href="mailto:info@tekscan.ru">info@tekscan.ru</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script type="text/javascript" src="https://api-maps.yandex.ru/2.1/?lang=ru_RU"></script>
+    <script type="text/javascript">
+        ymaps.ready(function () {
+            var myMap = new ymaps.Map('map', {
+                    center: [55.761684568962124, 37.65852649999999],
+                    zoom: 17
+                }, {
+                    searchControlProvider: 'yandex#search'
+                }),
+                MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+                    '<div style="color: #000000; font-weight: bold;">$[properties.iconContent]</div>'
+                ),
 
-<h2>Телефоны</h2>
+                myPlacemark = new ymaps.Placemark([55.761684568962124, 37.65852649999999], {
+                    hintContent: 'ул. Земляной Вал, 9, этаж 4, офис 4065 (БЦ Ситидел),  Москва, Россия',
+                    balloonContent: ''
+                }, {
+                    // Опции.
+                    // Необходимо указать данный тип макета.
+                    iconLayout: 'default#image',
+                    // Своё изображение иконки метки.
+                    iconImageHref: '/local/templates/divier/images/svg/map.svg',
+                    // Размеры метки.
+                    iconImageSize: [51, 65],
+                    // Смещение левого верхнего угла иконки относительно
+                    // её "ножки" (точки привязки).
+                    iconImageOffset: [-5, -38]
+                });
+            myMap.behaviors.disable('scrollZoom');
+            myMap.geoObjects.add(myPlacemark);
 
-<ul>
-  <li>Телефон/факс: 
-    <ul>
-      <li><b>(495) 212-85-06</b> </li>
-    </ul>
-  </li>
-
-  <li>Телефоны: 
-    <ul>
-      <li><b>(495) 212-85-07</b> </li>
-    
-      <li><b>(495) 212-85-08</b> </li>
-    </ul>
-  </li>
-</ul>
-
-<h2>Наш офис в Москве</h2>
-
-<p><?$APPLICATION->IncludeComponent("bitrix:map.google.view", ".default", array(
-	"KEY" => "ABQIAAAAOSNukcWVjXaGbDo6npRDcxS1yLxjXbTnpHav15fICwCqFS-qhhSby0EyD6rK_qL4vuBSKpeCz5cOjw",
-	"INIT_MAP_TYPE" => "NORMAL",
-	"MAP_DATA" => "a:3:{s:10:\"google_lat\";s:7:\"55.7383\";s:10:\"google_lon\";s:7:\"37.5946\";s:12:\"google_scale\";i:13;}",
-	"MAP_WIDTH" => "600",
-	"MAP_HEIGHT" => "500",
-	"CONTROLS" => array(
-		0 => "LARGE_MAP_CONTROL",
-		1 => "MINIMAP",
-		2 => "HTYPECONTROL",
-		3 => "SCALELINE",
-	),
-	"OPTIONS" => array(
-		0 => "ENABLE_SCROLL_ZOOM",
-		1 => "ENABLE_DBLCLICK_ZOOM",
-		2 => "ENABLE_DRAGGING",
-	),
-	"MAP_ID" => ""
-	),
-	false
-);?></p>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+        });
+    </script>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
